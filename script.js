315b6227cda91150c5fc9b4a34aba1cf6300e1e6
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-07-20 10:17:21 +0300

    Setup basic structure

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/*
 const test = document.querySelector("div");
 const newTest = document.createElement("div");
 newTest.textContent = "JS-Test";
 //test.insertAdjacentElement("afterend", newTest);
 test.parentElement.appendChild(newTest);
+*/
